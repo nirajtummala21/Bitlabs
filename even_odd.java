8c39e000c741d79b78a16e662b75da00d03691b2
sespl <sespl@sespls-Mac-mini-2.local>
2022-03-26 13:06:15 +0530

    .

---
+++ even_odd.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class even_odd{
+ public static void main(String args[]){
+  Scanner s=new Scanner(System.in);
+
+  int num=s.nextInt();
+  if(num%2==0)
+   System.out.println("number is even");
+  else
+   System.out.println("number is odd");
+ }
+}
+

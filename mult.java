2694850839e52fa617a7b91cec63fc520c770d6e
sespl <sespl@sespls-Mac-mini-2.local>
2022-03-26 17:51:19 +0530

    .

---
+++ mult.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class mult{
+ public static void main(String args[]){
+  Scanner s= new Scanner(System.in);
+  int num=s.nextInt();
+  for(int i=1;i<=10;i++)
+   System.out.println(+num*i);
+ }
+}
+
+
